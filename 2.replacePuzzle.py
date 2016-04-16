932f92e0486ff3c4d73a416a229314d163d5cedb
Kaya M Baber <kaya.baber@gmail.com>
2016-04-16 10:40:09 -0700

    First commit of python challenge puzzles

---
+++ 2.replacePuzzle.py
@@ -0,0 +1,16 @@
+#Python challenge 2 - http://www.pythonchallenge.com/pc/def/map.html
+
+oldinStr="g fmnc wms bgblr rpylqjyrc gr zw fylb. rfyrq ufyr amknsrcpq ypc dmp. bmgle gr gl zw fylb gq glcddgagclr ylb rfyr'q ufw rfgq rcvr gq qm jmle. sqgle qrpgle.kyicrpylq() gq pcamkkclbcb. lmu ynnjw ml rfc spj"
+inStr=" http://www.pythonchallenge.com/pc/def/map.html"
+
+inLis= list(inStr)
+outLis= list(inStr)
+keyStr="abcdefghijklmnopqrstuvwxyz"
+keyLis=list(keyStr)
+for r in range(len(inLis)):
+    for i in range(26):
+        if inLis[r] == keyLis[i]:
+            #print(inLis[r])
+            outLis[r] = keyLis[(i+2)%26]
+print(inStr + "\n")
+print(''.join(outLis))
---
+++ 3.rareCharacters.py
@@ -0,0 +1,3 @@
+#Python Challenge 3 - Find the rare characters in 3.rareCharacters.input.txt
+#http://www.pythonchallenge.com/pc/def/ocr.html
+
